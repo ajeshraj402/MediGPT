8d71b1bab938d296bee147e7979a8babda71e6d7
Ajesh Thangaraj Nadar <ajeshraj402@gmail.com>
2025-06-16 22:51:01 +0530

    embedder.py

--- utils/embedder.py
+++ utils/embedder.py
@@ -16,7 +16,7 @@ def chunk_text(pages, chunk_size=150):
     return chunks
 
 def get_embedder(model_name="all-MiniLM-L6-v2"):
-    return SentenceTransformer(model_name, device="cpu")
+    return SentenceTransformer(model_name)
 
 def embed_chunks(chunks, model):
     texts = [chunk['chunk'] for chunk in chunks]
